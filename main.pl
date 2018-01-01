4578ae77667d9bd94f7d6721959ae7910cad8ba9
Jacek Kierznikiewicz <thabriss@users.noreply.github.com>
2018-01-01 12:10:54 +0100

    dodano obliczanie PH i PP

--- main.pl
+++ main.pl
@@ -7,9 +7,12 @@ use List::MoreUtils qw(uniq); #pozwoli na sprawdzenie wystepowania duplikatow w 
 
 print "Witamy w programie wspomagajacym licytacje brydzowa!\n";
 
+my $pozycjaGracza;
+my $pozycjaRozdajacego;
+
 while(1) {	#gracz podaje swoja pozycje - zmienna pozycjaGracza
 	print "Podaj swoja pozycje (NEWS): ";
-	my $pozycjaGracza = <STDIN>;
+	$pozycjaGracza = <STDIN>;
 	chomp $pozycjaGracza;
 	$pozycjaGracza = uc $pozycjaGracza;
 	if ($pozycjaGracza =~ /[NEWS]/) {
@@ -21,7 +24,7 @@ while(1) {	#gracz podaje swoja pozycje - zmienna pozycjaGracza
 
 while(1) {	#gracz podaje pozycje rozdajacego - zmienna pozycjaRozdajacego
 	print "Podaj pozycje rozdajacego (NEWS): ";
-	my $pozycjaRozdajacego = <STDIN>;
+	$pozycjaRozdajacego = <STDIN>;
 	chomp $pozycjaRozdajacego;
 	$pozycjaRozdajacego = uc $pozycjaRozdajacego;
 	if ($pozycjaRozdajacego =~ /[NEWS]/) {
@@ -32,21 +35,30 @@ while(1) {	#gracz podaje pozycje rozdajacego - zmienna pozycjaRozdajacego
 }
 
 #pobranie reki od uzytkownika
+
+my $rekaString;
+my @rekaTablica;
+my $ilePoprawnych;
+my $ileKart;
+my @rekaUnikalne;
+my $ileKartUnik;
+my $ileNiepoprawnych;
+
 while(1) {
 	print "Podaj swoja reke (karty oddziel spacja):\n";
-	my $rekaString = <STDIN>;
+	$rekaString = <STDIN>;
 	$rekaString = uc $rekaString;
-	my @rekaTablica = split / /, $rekaString;
-	my $ilePoprawnych = 0; #ile kart zostalo podanych zgodnie z instrukcja
-	my $ileKart = $#rekaTablica + 1; #ile kart zostalo podane
-	my @rekaUnikalne = uniq @rekaTablica;
-	my $ileKartUnik = $#rekaUnikalne + 1; #ile kart sie nie powtarza
+	@rekaTablica = split / /, $rekaString;
+	$ilePoprawnych = 0; #ile kart zostalo podanych zgodnie z instrukcja
+	$ileKart = $#rekaTablica + 1; #ile kart zostalo podane
+	@rekaUnikalne = uniq @rekaTablica;
+	$ileKartUnik = $#rekaUnikalne + 1; #ile kart sie nie powtarza
 	if ($ileKartUnik == 13) {
 		foreach (@rekaTablica) {
 			if ($_ =~ /(10|[AKQJ2-9])[CDHS]/) { $ilePoprawnych++ }
 		}	
 		if ($ilePoprawnych != 13) {
-			my $ileNiepoprawnych = 13-$ilePoprawnych;
+			$ileNiepoprawnych = 13-$ilePoprawnych;
 			print "Znaleziono $ileNiepoprawnych niepoprawnych kart!\n";
 		} else {
 			last;
@@ -55,3 +67,43 @@ while(1) {
 		print "Bledna liczba kart! (za malo/duzo lub powtorzenia)\n"
 	}
 }
+
+#-------------
+#obliczanie potrzebnych wskaznikow
+#-------------
+
+my $punktyHonorowe = 0;
+my $punktyPrzeliczeniowe = 0;
+my $ileC = 0; #ile trefli na rece
+my $ileD = 0; #ile karo
+my $ileH = 0; #ile kierow
+my $ileS = 0; #ile pikow
+my $ppC = 0;
+my $ppD = 0;
+my $ppH = 0;
+my $ppS = 0;
+foreach (@rekaTablica) {
+	#punkty honorowe
+	if ($_ =~ /A/) {$punktyHonorowe += 4;} #za asy
+	if ($_ =~ /K/) {$punktyHonorowe += 3;} #za krole
+	if ($_ =~ /Q/) {$punktyHonorowe += 2;} #za damy
+	if ($_ =~ /J/) {$punktyHonorowe += 1;} #za walety
+
+	#dlugosc kolorow
+	if ($_ =~ /C/) {$ileC += 1;} #ile trefli
+	if ($_ =~ /D/) {$ileD += 1;} #ile karo
+	if ($_ =~ /H/) {$ileH += 1;} #ile kierow
+	if ($_ =~ /S/) {$ileS += 1;} #ile pikow
+}
+
+#PP za dlugosc
+if ($ileC > 4) {$ppC = $ileC - 4;}
+if ($ileD > 4) {$ppD = $ileD - 4;}
+if ($ileH > 4) {$ppH = $ileH - 4;}
+if ($ileS > 4) {$ppS = $ileS - 4;}
+
+#punkty przeliczeniowe
+$punktyPrzeliczeniowe = $punktyHonorowe + $ppC + $ppD + $ppH + $ppS;
+
+print "Punkty honorowe: $punktyHonorowe \n";
+print "Punkty przeliczeniowe: $punktyPrzeliczeniowe \n";
